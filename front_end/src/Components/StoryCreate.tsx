8a4e233be5e7a294263d1deb92847de84b0abf86
sananeminli <57816597+sananeminli@users.noreply.github.com>
2023-04-29 00:05:37 +0300

    RichText editor added #28

--- front_end/src/Components/StoryCreate.tsx
+++ front_end/src/Components/StoryCreate.tsx
@@ -1,10 +1,19 @@
 import React, { useState, useRef, ChangeEvent } from "react";
-import { GoogleMap, LoadScript, Marker, Autocomplete } from "@react-google-maps/api";
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'react-quill/dist/quill.snow.css';
+import {
+  GoogleMap,
+  LoadScript,
+  Marker,
+  Autocomplete,
+} from "@react-google-maps/api";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import ReactQuill from "react-quill";
+import { Col, Row, Container } from "react-bootstrap";
 
 const urlEndpoint = "http://localhost:8080/stories";
-const api_key   = import.meta.env.GOOGLE_API_KEY
+const api_key = import.meta.env.GOOGLE_API_KEY;
 const containerStyle = {
   width: "100%",
   height: "400px",
@@ -16,27 +25,33 @@ interface Location {
   lng: number;
 }
 
-
-
-interface Story{
-  text:string,
-  header:string,
-  labels:string[],
-  locations:Location[],
-  mediaString:string[]
+interface Story {
+  text: string;
+  header: string;
+  labels: string[];
+  locations: Location[];
+  mediaString: string[];
+  richText:string
 }
 
 const Story: React.FC = () => {
-  const [mapCenter, setMapCenter] = useState<{ lat: number; lng: number }>({ lat: 41.0856396, lng: 29.0424937 });
+  const [mapCenter, setMapCenter] = useState<{ lat: number; lng: number }>({
+    lat: 41.0856396,
+    lng: 29.0424937,
+  });
   const [locations, setLocations] = useState<Location[]>([]);
-  const [text, setText] = useState<string>('');
-  const [header, setHeader] = useState<string>('');
+  const [text, setText] = useState<string>("");
+  const [header, setHeader] = useState<string>("");
   const [labels, setLabels] = useState<string[]>([]);
-  const [media, setMedia] = useState<string[]>([])
-  const navigate = useNavigate()
-  
-  const autocompleteRef = useRef<google.maps.places.Autocomplete | null>(null);
+  const [media, setMedia] = useState<string[]>([]);
+  const [editorContent, setEditorContent] = useState("");
 
+  const navigate = useNavigate();
+ 
+  const autocompleteRef = useRef<google.maps.places.Autocomplete | null>(null);
+  const handleEditorChange = (content: string) => {
+    setEditorContent(content);
+  };
   const handleLocationSelect = () => {
     const place = autocompleteRef.current?.getPlace();
     if (place?.geometry?.location) {
@@ -45,9 +60,9 @@ const Story: React.FC = () => {
         lat: Number(place.geometry?.location?.lat().toFixed(6)),
         lng: Number(place.geometry?.location?.lng().toFixed(6)),
       };
-      
+
       setLocations([...locations, locationData]);
-      console.log(locationData)
+      console.log(locationData);
       setMapCenter({ lat: locationData.lat, lng: locationData.lng });
     }
   };
@@ -57,7 +72,9 @@ const Story: React.FC = () => {
     const lat = latLng?.lat();
     const lng = latLng?.lng();
     try {
-      const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${api_key}`);
+      const response = await axios.get(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${api_key}`
+      );
       const { results } = response.data;
       if (results.length > 0) {
         const locationData: Location = {
@@ -73,40 +90,41 @@ const Story: React.FC = () => {
     }
   };
 
-  const handleSubmit = () =>{
-    
+  const handleSubmit = () => {
     const storyRequest: Story = {
       text,
       header,
       labels,
       locations,
-      mediaString: media
+      mediaString: media,
+      richText:editorContent
     };
     async function postData() {
       try {
-        console.log(storyRequest)
-        const response = await axios.post('http://localhost:8080/stories', storyRequest, {
-          withCredentials: true
-        });
-        
-        console.log(response)
+        console.log(storyRequest);
+        const response = await axios.post(
+          "http://localhost:8080/stories",
+          storyRequest,
+          {
+            withCredentials: true,
+          }
+        );
+
+        console.log(response);
       } catch (error) {
-        console.error('Error:', error);
+        console.error("Error:", error);
       }
     }
-    
-    postData();
-    navigate("/home",{replace:true})
-    
-    
 
+    postData();
+    navigate("/home", { replace: true });
   };
 
   const blobToBase64 = (blob: Blob): Promise<string> => {
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
       reader.onload = () => {
-        const base64Data = (reader.result as string).split(',')[1]; // Get the base64 part without the data URL prefix
+        const base64Data = (reader.result as string).split(",")[1]; // Get the base64 part without the data URL prefix
         resolve(base64Data);
       };
       reader.onerror = (error) => {
@@ -115,7 +133,6 @@ const Story: React.FC = () => {
       reader.readAsDataURL(blob);
     });
   };
-
 
   const handleTextChange = (event: ChangeEvent<HTMLInputElement>) => {
     setText(event.target.value);
@@ -126,72 +143,160 @@ const Story: React.FC = () => {
   };
 
   const handleLabelsChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setLabels(event.target.value.split(',').map((label) => label.trim()));
+    setLabels(event.target.value.split(",").map((label) => label.trim()));
   };
 
   const handleFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
       const blobs = Array.from(event.target.files).map((file) => file.slice(0));
       const base64Strings = await Promise.all(blobs.map(blobToBase64));
-      
+
       console.log(base64Strings); // Array of base64-encoded strings
       setMedia(base64Strings);
     }
-  };
-  return (
-    <div>
-      <LoadScript googleMapsApiKey= {api_key} libraries={["places"]}>
-          <form >
 
-          <label>
-            Text:
-            <input type="text" value={text} onChange={handleTextChange} />
-          </label>
-          <label>
-            Header:
-            <input type="text" value={header} onChange={handleHeaderChange} />
-          </label>
-          <label>
-            Labels (comma-separated):
-            <input type="text" value={labels.join(', ')} onChange={handleLabelsChange} />
-          </label>
-          <input type="file" onChange={handleFileChange} multiple required />
-          
-          <button type="submit" onClick={handleSubmit}>Submit</button>
-          
-        </form>
-        <div className="form-group">
-          <label>Locations:</label>
-          <Autocomplete
-            onLoad={(autocomplete) => {
-              autocompleteRef.current = autocomplete;
-            }}
-            onPlaceChanged={handleLocationSelect}
-          >
-            <input type="text" className="form-control" />
-          </Autocomplete>
-          <ul>
-            {locations.map((loc, index) => (
-              <div key={index}>
-                <li>{loc.name || `${loc.lat}, ${loc.lng}`}</li>
-                <button type="button" onClick={() => setLocations(locations.filter((_, i) => i !== index))}>Remove</button>
+
+  };
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["link", "image"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col >
+            <ReactQuill value={editorContent} onChange={handleEditorChange} theme="snow" />
+          </Col>
+          <Col>
+            <Row >
+              <div className="form-group">
+                <label>Locations:</label>
+                <Autocomplete
+                  onLoad={(autocomplete) => {
+                    autocompleteRef.current = autocomplete;
+                  }}
+                  onPlaceChanged={handleLocationSelect}
+                >
+                  <input type="text" className="form-control" />
+                </Autocomplete>
+                <ul>
+                  {locations.map((loc, index) => (
+                    <div key={index}>
+                      <li>{loc.name || `${loc.lat}, ${loc.lng}`}</li>
+                      <button
+                        type="button"
+                        className="btn btn-secondary"
+                        onClick={() =>
+                          setLocations(locations.filter((_, i) => i !== index))
+                        }
+                      >
+                        Remove
+                      </button>
+                    </div>
+                  ))}
+                </ul>
               </div>
-            ))}
-          </ul>
-        </div>
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={mapCenter}
-          zoom={20}
-          onClick={handleMapClick}
-        >
-          {locations.map((loc, index) => (
-            <Marker key={index} position={{ lat: loc.lat, lng: loc.lng }} />
-          ))}
-        </GoogleMap>
-        
-      </LoadScript>
-    </div>
+            </Row>
+            <Row>
+            <ReactQuill
+              theme="snow"
+              value={editorContent}
+              onChange={handleEditorChange}
+              formats={formats}
+              modules={modules}
+            />
+            </Row>
+            <Row>
+              <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={mapCenter}
+                zoom={20}
+                onClick={handleMapClick}
+              >
+                {locations.map((loc, index) => (
+                  <Marker
+                    key={index}
+                    position={{ lat: loc.lat, lng: loc.lng }}
+                  />
+                ))}
+              </GoogleMap>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <Col>
+            <form>
+              <div className="form-group">
+                <label>Text:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={text}
+                  onChange={handleTextChange}
+                />
+              </div>
+              <div className="form-group">
+                <label>Header:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={header}
+                  onChange={handleHeaderChange}
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Labels (comma-separated):</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={labels.join(", ")}
+                  onChange={handleLabelsChange}
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="btn btn-primary"
+                onClick={handleSubmit}
+              >
+                Submit
+              </button>
+            </form>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="row">
+              <div className="col-md-8"></div>
+              <div className="col-md-4"></div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
