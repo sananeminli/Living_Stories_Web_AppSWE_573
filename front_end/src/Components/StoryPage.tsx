13bc57512b5887773dee6d7d56b8f953519bf8d3
sananeminli <57816597+sananeminli@users.noreply.github.com>
2023-04-29 21:43:53 +0300

    Done.#34

--- front_end/src/Components/StoryPage.tsx
+++ front_end/src/Components/StoryPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { useParams, Params } from "react-router-dom";
 import { StoryInt } from "../Interfaces/StoryInt";
 import axios from "axios";
@@ -8,19 +8,66 @@ import "react-quill/dist/quill.snow.css";
 import { Col, Container, Row } from "react-bootstrap";
 import RichTextPreview from "./RichTextPreview";
 import NavBar from "./NavBar";
+import { GoogleMap, Marker } from "@react-google-maps/api";
 
 interface StoryPageProps {
   story: StoryInt;
 }
+const containerStyle = {
+  width: "100%",
+  height: "400px",
+};
 
 interface RouteParams extends Params {
   id: string;
 }
+interface Location {
+  lat: number;
+  lng: number;
+  name:string
+}
+
+interface LocationProps {
+  slocations: Location[];
+}
+
 
 const StoryPage: React.FC<StoryPageProps> = ({ story }) => {
+  const StoryMarkers: FC<LocationProps> = ({ slocations }) => {
+    const markers = slocations.map((location, index) => (
+      <Marker
+        key={index}
+        position={{
+          lat: location.lat,
+          lng: location.lng,
+        }}
+      />
+    ));
+  
+    return <>{markers}</>;
+  };
+
+  console.log(story)
+
+  let latSum = 0;
+  let lngSum = 0;
+
+  // Loop through locations
+  story.locations.forEach((loc) => {
+    latSum += loc.lat;
+    lngSum += loc.lng;
+  });
+
+  // Calculate average coordinates
+  const latAvg = latSum / story.locations.length;
+  const lngAvg = lngSum / story.locations.length;
+
+  // Set map center
+  const mapCenter = { lat: latAvg, lng: lngAvg };
+
   return (
     <>
-      <NavBar/>
+      <NavBar />
       <Container>
         <Row>
           <h1>{story.header}</h1>
@@ -32,16 +79,38 @@ const StoryPage: React.FC<StoryPageProps> = ({ story }) => {
           <Col xs={3}>
             <RichTextPreview content={story.richText} />
           </Col>
+
+          <Col>
+            <GoogleMap
+              zoom={3}
+              mapContainerStyle={containerStyle}
+              center={mapCenter}
+            >
+              <StoryMarkers slocations = {story.locations}/>
+            </GoogleMap>
+          </Col>
+        </Row>
+        <p>Likes: {story.likes.length}</p>
+      </Container>
+      <Container>
+        <Row>
+        {story.comments.map((comment, index) => (
+          <div key={index}>
+            <p>{comment.text}</p>
+            <p>By: {comment.user.name}</p>
+            <p>Likes: {comment.likes.length}</p>
+          </div>
+        ))}
         </Row>
       </Container>
 
-      <p>Likes: {story.likes.length}</p>
+      
     </>
   );
 };
 
 const StoryPageContainer: React.FC = () => {
-  const [story, setStory] = useState<StoryInt | null>(null);
+  const [story, setStory] = useState<StoryInt >();
   const { id } = useParams<RouteParams>();
 
   useEffect(() => {
