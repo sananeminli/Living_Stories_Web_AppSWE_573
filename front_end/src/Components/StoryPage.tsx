b0e53e936f3546e2c11690b1e4d492994ff2959c
sananeminli <57816597+sananeminli@users.noreply.github.com>
2023-04-29 19:30:09 +0300

    Story details page have been created#33

--- front_end/src/Components/StoryPage.tsx
+++ front_end/src/Components/StoryPage.tsx
@@ -1,7 +1,13 @@
-import { useEffect, useState } from 'react';
-import { useParams, Params } from 'react-router-dom';
-import { StoryInt } from '../Interfaces/StoryInt';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import { useParams, Params } from "react-router-dom";
+import { StoryInt } from "../Interfaces/StoryInt";
+import axios from "axios";
+import ReactQuill from "react-quill";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-quill/dist/quill.snow.css";
+import { Col, Container, Row } from "react-bootstrap";
+import RichTextPreview from "./RichTextPreview";
+import NavBar from "./NavBar";
 
 interface StoryPageProps {
   story: StoryInt;
@@ -13,12 +19,24 @@ interface RouteParams extends Params {
 
 const StoryPage: React.FC<StoryPageProps> = ({ story }) => {
   return (
-    <div>
-      <h1>{story.header}</h1>
-      <p>By: {story.user.name}</p>
+    <>
+      <NavBar/>
+      <Container>
+        <Row>
+          <h1>{story.header}</h1>
+        </Row>
+        <Row>
+          <p>By: {story.user.name}</p>
+        </Row>
+        <Row>
+          <Col xs={3}>
+            <RichTextPreview content={story.richText} />
+          </Col>
+        </Row>
+      </Container>
+
       <p>Likes: {story.likes.length}</p>
-      <p>{story.text}</p>
-    </div>
+    </>
   );
 };
 
@@ -29,7 +47,9 @@ const StoryPageContainer: React.FC = () => {
   useEffect(() => {
     // Fetch story data from API using the ID parameter
     const fetchStory = async () => {
-      const response = await axios.get(`http://localhost:8080/stories/${id}`);
+      const response = await axios.get(`http://localhost:8080/stories/${id}`, {
+        withCredentials: true,
+      });
       setStory(response.data);
     };
 
