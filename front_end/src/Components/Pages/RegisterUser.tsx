bd27ff17627a66a911454fd4adc77f821459e735
sananeminli <57816597+sananeminli@users.noreply.github.com>
2023-05-23 13:31:40 +0300

    Done. closes #57

--- front_end/src/Components/Pages/RegisterUser.tsx
+++ front_end/src/Components/Pages/RegisterUser.tsx
@@ -1,5 +1,10 @@
+import Button from "antd/es/button";
+import Form from "antd/es/form";
+import Input from "antd/es/input";
+import Image from "antd/es/image";
 import axios from "axios";
 import { useState } from "react";
+import { Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import {useNavigate} from 'react-router-dom';
 
@@ -17,14 +22,14 @@ const RegisterUser: React.FC = () => {
   const navigate = useNavigate();
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+    
     if (formData.password !== formData.repassword) {
       setPasswordError(true);
     } else {
       setPasswordError(false);
       // Send form data to backend
       try {
-        const response = await axios.post('http://localhost:8080/users', formData);
+        const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/users`, formData);
         console.log(response.data);
         navigate('/login',{replace: true})
         
@@ -50,29 +55,66 @@ const RegisterUser: React.FC = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" value={formData.email} onChange={handleEmailChange} />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" value={formData.password} onChange={handlePasswordChange} />
-      </div>
-      <div>
-        <label htmlFor="repassword">Confirm Password:</label>
-        <input type="password" id="repassword" name="repassword" value={formData.repassword} onChange={handleRepasswordChange} />
-      </div>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" value={formData.name} onChange={handleNameChange} />
-      </div>
-      {passwordError && <p style={{ color: 'red' }}>Password should be same</p>}
-      
-      <button type="submit">Submit</button>
+    
+    
+      <div className="login-page">
+        <img src="./src/assets/images/logo_kare.png"  style={{ width:"50%" , height:"auto" }}/>
         
-    </form>
-  );
-};
+        <Row style={{ justifyContent: "center", marginTop: "50px" }}>
+          <Form
+            name="control-ref"
+            onFinish={handleSubmit}
+            style={{ maxWidth: 600 }}
+          >
+            <Form.Item name="Email" label="Email" rules={[{ required: true }]}>
+              <Input onChange={handleEmailChange} />
+            </Form.Item>
+            <Form.Item name="name" label="Name" rules={[{ required: true }]}>
+              <Input onChange={handleNameChange} />
+            </Form.Item>
+
+            <Form.Item
+              name="password"
+              label="Password"
+              rules={[{ required: true }]}
+            >
+              <Input.Password
+                placeholder="input password"
+                onChange={handlePasswordChange}
+              />
+            </Form.Item>
+            <Form.Item 
+            name="repassword"
+            label = "Repeat Password"
+            rules={[
+                                {
+                                    required: true,
+                                    message: "You must confirm your password.",
+                                },
+                                ({ getFieldValue }) => ({
+                                    validator(_, value) {
+                                        if (!value || getFieldValue("password") === value) {
+                                            return Promise.resolve();
+                                        }
+                                        return Promise.reject(
+                                            new Error("The passwords do not match.")
+                                        );
+                                    },
+                                }),
+                            ]}>
+              <Input.Password   placeholder="repeat password" onChange={handleRepasswordChange}/>
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Register
+              </Button>
+            </Form.Item>
+          </Form>
+        </Row>
+      </div>
+    );
+  };
+    
+ 
 
 export default RegisterUser;
---
+++ living_stories/src/test/java/com/swe573/living_stories/Services/CommentServiceTest.java
@@ -0,0 +1,18 @@
+package com.swe573.living_stories.Services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentServiceTest {
+
+    @Test
+    void addCommentToStory() {
+    }
+
+    
+
+    @Test
+    void likeComment() {
+    }
+}
