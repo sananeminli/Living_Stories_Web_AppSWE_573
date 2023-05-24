d80b9d2fea6c8f501fb96fb609c2d9d532aaa3fd
sananeminli <57816597+sananeminli@users.noreply.github.com>
2023-05-24 04:55:19 +0300

    Dockerized front end. Close #62

--- front_end/vite.config.ts
+++ front_end/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    
+    host: true,
+    strictPort : true,
+    port: 3000, 
+    
+}})
